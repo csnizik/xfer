003402c8f43aa0fa8462484f7dd15724e80a0128
Marcos Carballal <marcos.carballal@spatialfront.com>
2022-02-02 19:53:38 -0500

    Renamed Hook test to CIG PODS

---
+++ src/Plugin/Asset/AssetType/CigProject.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\cig-pods\Plugin\Asset\AssetType;
+
+use Drupal\farm_entity\Plugin\Asset\AssetType\FarmAssetType;
+/**
+   * Provides the CIG Project asset type.
+   *
+   * @AssetType(
+   * id = "cig_project",
+   * label = @Translation("CIG Project"),
+   * )
+   */
+class CigProject extends FarmAssetType {
+
+}

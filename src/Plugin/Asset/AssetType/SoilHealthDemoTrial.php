725f69ac7b53ef4b83af0c94a206a5b2b2781a9d
evan.kelley@spatialfront.com <ekelley@ChiefSystem.localdomain>
2022-02-08 13:46:41 -0800

    added fields to the Soil Health Demo Trials asset

---
+++ src/Plugin/Asset/AssetType/SoilHealthDemoTrial.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\cig_pods\Plugin\Asset\AssetType;
+
+use Drupal\farm_entity\Plugin\Asset\AssetType\FarmAssetType;
+/**
+   * Provides the CIG Project asset type.
+   *
+   * @AssetType(
+   * id = "soil_health_demo_trial",
+   * label = @Translation("Soil Health Demo Trial"),
+   * )
+   */
+class SoilHealthDemoTrial extends FarmAssetType {
+
+}

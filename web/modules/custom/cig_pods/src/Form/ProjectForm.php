be64ed583f8d47a17e7b9510db3d038e030b9129
Marcos Carballal <marcoscrballal@spatialfront.com>
2022-05-25 13:29:20 -0400

    Grant Types, Resource Concerns, and awardees now populate dynamically

--- web/modules/custom/cig_pods/src/Form/ProjectForm.php
+++ web/modules/custom/cig_pods/src/Form/ProjectForm.php
@@ -7,10 +7,6 @@ Use Drupal\Core\Form\FormStateInterface;
 
 class ProjectForm extends FormBase {
 
-   
-	/*
-		Take in form and form_state using pass by reference on the form. No need to return the form
-	*/
 	public function buildProjectInformationSection(array &$form, FormStateInterface &$form_state, $options = NULL){
 		$form['form_title'] = [
 			'#markup' => '<h1 id="form-title">Project Information</h1>'
@@ -33,6 +29,31 @@ class ProjectForm extends FormBase {
 			'$description' => 'Agreement Number',
 			'#required' => FALSE
 		];
+
+		// Start: Taxonomy term loaded dynamically example
+
+		$grant_type_terms = \Drupal::entityTypeManager() -> getStorage('taxonomy_term') -> loadByProperties(
+			[
+				'vid' => 'd_grant_type',
+			]
+		);
+
+		$grant_type_options = [];
+		$grant_type_keys = array_keys($grant_type_terms);
+
+		foreach($grant_type_keys as $grant_type_key) {
+			$term = $grant_type_terms[$grant_type_key];
+			$grant_type_options[$grant_type_key] = $term -> getName();
+		}
+		
+		$form['grant_type'] = [
+			'#type' => 'select',
+			'#title' => $this
+			  ->t('Grant Type'),
+			'#options' => $grant_type_options,
+			'#required' => FALSE
+		];
+		// End: Taxonomy term loaded dynamically example
 
 		$form['funding_amount'] = [
 			'#type' => 'textfield',
@@ -70,38 +91,159 @@ class ProjectForm extends FormBase {
 			'#required' => FALSE
 		];
 
-		// Start: Taxonomy term loaded dynamically example
 
-		$grant_type_terms = \Drupal::entityTypeManager() -> getStorage('taxonomy_term') -> loadByProperties(
-			[
-				'vid' => 'd_grant_type',
-			]
-		);
-
-		$grant_type_options = [];
-		$grant_type_keys = array_keys($grant_type_terms);
-
-		foreach($grant_type_keys as $grant_type_key) {
-			$term = $grant_type_terms[$grant_type_key];
-			$grant_type_options[$grant_type_key] = $term -> getName();
-		}
-		
-		$form['grant_type'] = [
-			'#type' => 'select',
-			'#title' => $this
-			  ->t('Grant Type'),
-			'#options' => $grant_type_options,
-			'#required' => FALSE
-		];
-		// End: Taxonomy term loaded dynamically example
 
 
 	}
 
-	/*
-		Take in form and form_state using pass by reference on the form. No need to return the form
-	*/
-	public function buildProducerInformationSection(array &$form, FormStateInterface &$form_state, $options = NULL){
+	public function getAwardeeOptions(){
+		$awardee_assets = \Drupal::entityTypeManager() -> getStorage('asset') -> loadByProperties(
+			['type' => 'awardee']
+		);
+		$awardee_options = array();
+		$awardee_keys = array_keys($awardee_assets);
+		foreach($awardee_keys as $awardee_key) {
+		  $asset = $awardee_assets[$awardee_key];
+		  $awardee_options[$awardee_key] = $asset->getName();
+		}
+
+		return $awardee_options;
+	}
+
+
+	# Making this function dynamically pull contact names breaks AJAX calls.
+	public function getAwardeeContactNameOptions(){
+		$contact_name_options = array();
+		$contact_name_options[''] = ' - Select -';
+		$contact_name_options['aw'] = 'Agatha Wallace';
+
+		return $contact_name_options;
+	}
+
+	// TODO: improvement: Make this dynamic
+	public function getAwardeeContactTypeOptions(){
+		// $contact_type_terms = \Drupal::entityTypeManager() -> getStorage('taxonomy_term') -> loadByProperties(
+		//    ['vid' => 'd_contact_type']
+		// );
+		// $contact_type_options = [];
+		// $contact_type_keys = array_keys($contact_type_terms);
+		// print_r($contact_type_keys);
+		// foreach($contact_type_keys as $contact_type_key) {
+		//   $term = $contact_type_terms[$contact_type_key];
+		//   $contact_type_options[$contact_type_key] = $term -> getName();
+		// }
+		$contact_type_options[''] = '- Select -';
+		$contact_type_options['0'] = 'Awardee Administrative Contact';
+		$contact_type_options['1'] = 'Awardee Principal Contact';
+		$contact_type_options['2'] = 'Awardee Technical Contact';
+
+
+		return $contact_type_options;
+	}
+	
+   /**
+   * {@inheritdoc}
+   */
+	public function buildForm(array $form, FormStateInterface $form_state, $options = NULL){
+
+		$form['#attached']['library'][] = 'cig_pods/project_entry_form';
+
+		$num_lines = $form_state->get('num_lines');//get num of contacts showing on screen. (1->n exclude:removed indexes)
+		$num_indexes = $form_state->get('num_indexes');//get num of added contacts. (1->n)
+
+		if ($num_indexes === NULL) {//initialize number of contact, set to 1
+			$form_state->set('num_indexes', 1);
+			$num_indexes = $form_state->get('num_indexes');
+		}
+		if ($num_lines === NULL) {
+			$form_state->set('num_lines', 1);
+			$num_lines = $form_state->get('num_lines');
+		}
+
+		$removed_fields = $form_state->get('removed_fields');//get removed contacts indexes
+		if ($removed_fields === NULL) {
+			$form_state->set('removed_fields', array());//initialize arr
+			$removed_fields = $form_state->get('removed_fields');
+		}
+		/* Variables declaration end*/
+
+		$this->buildProjectInformationSection($form, $form_state);
+
+		$awardee_options = $this->getAwardeeOptions();
+		$contact_name_options = $this->getAwardeeContactNameOptions();
+		$contact_type_options = $this->getAwardeeContactTypeOptions();
+		/* Awardee Information */
+		$form['subform_2'] = [
+			'#markup' => '<div class="subform-title-container"><h2>Awardee Information</h2><h4>Section 2213 of 3</h4></div>'
+		];
+
+		$form['organization_name'] = [
+			'#type' => 'select',
+			'#title' => 'Awardee Organization Name',
+			'#options' => $awardee_options,
+			'#required' => TRUE
+		];
+
+
+		// $contact_name_options = $this->getAwardeeContactNameOptions();
+		$form['#tree'] = TRUE;
+		$form['names_fieldset'] = [
+		  '#prefix' => '<div id="names-fieldset-wrapper"',
+		  '#suffix' => '</div>',
+		];
+
+
+
+		for ($i = 0; $i < $num_indexes; $i++) {//num_indexes: get num of added contacts. (1->n)
+
+			if (in_array($i, $removed_fields)) {// Check if field was removed
+				continue;
+			}
+
+			$form['names_fieldset'][$i]['contact_name'] = [
+				'#type' => 'select',
+				'#title' => $this
+				  ->t("Contact Name"),
+				'#options' => $contact_name_options,
+				'attributes' => [
+					'class' => 'something',
+				],
+				'#prefix' => ($num_lines > 1) ? '<div class="inline-components-short">' : '<div class="inline-components">',
+		  		'#suffix' => '</div>',
+			];
+
+			$form['names_fieldset'][$i]['contact_type'] = [
+				'#type' => 'select',
+				'#title' => $this
+				  ->t('Contact Type'),
+				'#options' => $contact_type_options,
+				'#prefix' => '<div class="inline-components"',
+		  		'#suffix' => '</div>',
+			];
+
+			if($num_lines > 1 && $i!=0){
+				$form['names_fieldset'][$i]['actions'] = [
+					'#type' => 'submit',
+					'#value' => $this->t('Delete'),
+					'#name' => $i,
+					'#submit' => ['::removeCallback'],
+					'#ajax' => [
+					  'callback' => '::addmoreCallback',
+					  'wrapper' => 'names-fieldset-wrapper',
+					],
+					"#limit_validation_errors" => array(),
+					'#prefix' => '<div class="remove-button-container">',
+					'#suffix' => '</div>',
+				];
+			}
+
+			//css space for a new line due to previous items' float left attr
+			$form['names_fieldset'][$i]['new_line_container'] = [
+				'#markup' => '<div class="clear-space"></div>'
+			];
+			
+		}
+
 		/* Producers Information */
 		$form['subform_3'] = [
 			'#markup' => '<div class="subform-title-container"><h2>Producers</h2><h4>Section 3 of 3</h4></div>'
@@ -111,155 +253,14 @@ class ProjectForm extends FormBase {
 			'#type' => 'textfield',
 			'#title' => $this->t('Producer Contact Name'),
 			'$description' => 'Producer Contact Name',
-			'#required' => FALSE
+			'#required' => TRUE
 		];
 		/* Producers Information Ends*/
-	}
 
-   /**
-   * {@inheritdoc}
-   */
-	public function buildForm(array $form, FormStateInterface $form_state, $options = NULL){
-
-		$form['#attached']['library'][] = 'cig_pods/project_entry_form';
-
-
-		// $this->buildProjectInformationSection($form, $form_state, $options);
-		
-		$form_state->set('num_indexes', $form_state->get('num_indexes') 	  == NULL ? 1 : $form_state->get('num_indexes') );
-		$form_state->set('num_lines', $form_state->get('num_lines') 		  == NULL ? 1 : $form_state->get('removed_fields') );
-		$form_state->set('removed_fields', $form_state->get('removed_fields') == NULL ? array() : $form_state->get('removed_fields') );
-
-		$num_indexes = $form_state->get('num_indexes');
-		$num_lines = $form_state->get('num_lines');
-		$removed_fields = $form_state->get('removed_fields');
-
-		// $removed_fields = $form_state->get('removed_fields');//get removed contacts indexes
-		/* Variables declaration end*/
-
-		/* Awardee Information */
-		$form['subform_2'] = [
-			'#markup' => '<div class="subform-title-container"><h2>Awardee Information</h2><h4>Section 2 of 3</h4></div>'
+		$form['actions'] = [
+			'#type' => 'actions',
 		];
 
-		/* Selection of existing Awardees*/
-
-		$awardee_assets = \Drupal::entityTypeManager() -> getStorage('asset') -> loadByProperties(
-			['type' => 'awardee']
-		);
-		$awardee_options = array();
-		$awardee_keys = array_keys($awardee_assets);
-		print_r($awardee_keys);
-		foreach($awardee_keys as $awardee_key) {
-		  $asset = $awardee_assets[$awardee_key];
-		  $awardee_options[$awardee_key] = $asset->getName();
-		}
-		$form['awardee'] = [
-		  '#type' => 'select',
-		  '#title' => t('Awardee Organization'),
-		  '#options' => $awardee_options,
-		  '#required' => FALSE
-		];
-
-
-		$form['create_organization'] = [
-			'#type' => 'button',
-			'#value' => $this->t('Create New Awardee Organization(s)'),
-			'#attributes' => array('onClick' => 'alert(1)'),
-		];
-
-		$form['#tree'] = TRUE; // TODO: figure out what this accomplishes
-		
-		$form['names_fieldset'] = [
-		  '#prefix' => '<div id="names-fieldset-wrapper"',
-		  '#suffix' => '</div>',
-		];
-
-		// Start: Load the list of existing contact types 
-
-		$contact_type_terms = \Drupal::entityTypeManager() -> getStorage('taxonomy_term') -> loadByProperties(
-			['vid' => 'd_contact_type']
-		);
-
-		$contact_type_options = [];
-		$contact_type_keys = array_keys($contact_type_terms);
-		foreach($contact_type_keys as $contact_type_key) {
-			$term = $contact_type_terms[$contact_type_key];
-			$contact_type_options[$contact_type_key] = $term -> getName();
-		}
-
-		// End: Load the list of existing contact types
-
-		$contact_type_form_element_settings = [
-			'#type' => 'select',
-			'#title' => $this->t('Contact Type'),
-			'#options' => $contact_type_options,
-			'#prefix' => '<div class="inline-components"',
-			'#suffix' => '</div>',
-		];
-
-		$contact_name_form_element_settings = [
-			'#type' => 'select',
-			'#title' => $this
-			  ->t("Contact Name"),
-			'#options' => [
-			  '' => $this
-				->t(' - Select - '),
-			  'dacn1' => $this
-				->t('Dummy awardee contact name 1'),
-			  'dacn2' => $this
-				->t('Dummy awardee contact name 2'),
-			  'dacn3' => $this
-				->t('Dummy awardee contact name 3'),
-			],
-			'attributes' => [
-				'class' => 'something',
-			],
-
-			// TODO: Check if this is the best way 
-			'#prefix' => ($num_lines > 1) ? '<div class="inline-components-short">' : '<div class="inline-components">',
-			  '#suffix' => '</div>',
-		];
-
-		$contact_delete_button_form_element_settings = [
-			'#type' => 'submit',
-			'#attributes' => array('onClick' => 'alert(1)'),
-			'#value' => $this->t('Delete'),
-			'#name' => 'delete_button',
-			'#submit' => ['::removeCallback'],
-			'#ajax' => [
-			  'callback' => '::addmoreCallback',
-			  'wrapper' => 'names-fieldset-wrapper',
-			],
-			"#limit_validation_errors" => array(),
-			'#prefix' => '<div class="remove-button-container">',
-			'#suffix' => '</div>',
-		];
-
-		// contact_delete_button_form_element_settings = []
-
-		// This is temporary, need to go 
-		for ($i = 0; $i < 10; $i++) {//num_indexes: get num of added contacts. (1->n)
-			print_r("Num indexes go brr");
-			if (in_array($i, $removed_fields)) {// Check if field was removed
-				continue;
-			}
-
-			$form['names_fieldset'][$i]['contact_name'] = $contact_name_form_element_settings;
-
-			$form['names_fieldset'][$i]['contact_type'] = $contact_type_form_element_settings;
-
-			$form['names_fieldset'][$i]['actions'] = $contact_delete_button_form_element_settings;
-
-			//css space for a new line due to previous items' float left attr
-			$form['names_fieldset'][$i]['new_line_container'] = [
-				'#markup' => '<div class="clear-space"></div>'
-			];
-			
-		}
-
-		
-		
 		$form['names_fieldset']['actions']['add_name'] = [
 			'#type' => 'submit',
 			'#button_type' => 'button',
@@ -272,16 +273,16 @@ class ProjectForm extends FormBase {
 			],
 			'#states' => [
 				'visible' => [
-					":input[name='names_fieldset[0][contact_name]']" => ['!value' => ''],
-					"and",
-					":input[name='names_fieldset[0][contact_type]']" => ['!value' => ''],
+				  ":input[name='names_fieldset[0][contact_name]']" => ['!value' => ''],
+				  "and",
+				  ":input[name='names_fieldset[0][contact_type]']" => ['!value' => ''],
 				],
 			],
 			"#limit_validation_errors" => array(),
 			'#prefix' => '<div id="addmore-button-container">',
 			'#suffix' => '</div>',
 		];
-		
+
 		// $this->buildProducerInformationSection($form, $form_state, $options);
 		$form_state->setCached(FALSE);
 
@@ -309,10 +310,7 @@ class ProjectForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-
-	$this->messenger()->addStatus($this->t('Form submitted!'));
-
-	if($form_state->getValues()['actions']['#value'] == t('Save')){
+	if($form_state['values']['op'] == t('Save')){
 		$this
 		->messenger()
 		->addStatus($this
@@ -334,26 +332,16 @@ class ProjectForm extends FormBase {
     return $form['names_fieldset'];
   }
 
-  public function addOne(array &$form, FormStateInterface &$form_state) {
-	print_r("BRRRR");
-	$this->messenger()->addStatus('BRRRR');
-    $num_indexes = $form_state->get('num_indexes');
-	$num_lines = $form_state->get('num_lines');
-    $form_state->set('num_indexes', $num_indexes + 1);
-	$form_state->set('num_lines', $num_lines + 1);
+  public function addOne(array &$form, FormStateInterface $form_state) {
+    $num_field = $form_state->get('num_indexes');
+	$num_line = $form_state->get('num_lines');
+    $add_button = $num_field + 1;
+    $form_state->set('num_indexes', $add_button);
+	$form_state->set('num_lines', $num_line + 1);
     $form_state->setRebuild();
   }
 
-
-
-  public function removeContactFromList(array &$form, FormStateInterface &$form_state){
-	  $trigger = $form_state->getTriggeringElement();
-  }
-
-
-
-  public function removeCallback(array &$form, FormStateInterface &$form_state) {
-	print_r("Removecallbackgobrrr");
+  public function removeCallback(array &$form, FormStateInterface $form_state) {
     $trigger = $form_state->getTriggeringElement();
 	$num_line = $form_state->get('num_lines');
     $indexToRemove = $trigger['#name'];

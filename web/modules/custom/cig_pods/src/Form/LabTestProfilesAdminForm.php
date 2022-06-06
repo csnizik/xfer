08bd8f7423522082fe4fe8092afe8c12629226cc
Mac <mac@MacBook-Pro.local>
2022-06-06 15:48:35 -0700

    added taxonomy logic and new snippets

--- web/modules/custom/cig_pods/src/Form/LabTestProfilesAdminForm.php
+++ web/modules/custom/cig_pods/src/Form/LabTestProfilesAdminForm.php
@@ -4,27 +4,24 @@ namespace Drupal\cig_pods\Form;
 
 Use Drupal\Core\Form\FormBase;
 Use Drupal\Core\Form\FormStateInterface;
+Use Drupal\asset\Entity\Asset;
 
 class LabTestProfilesAdminForm extends FormBase {
 
-    public function getSoilHealthExtractionOptions(){
+    public function getSoilHealthExtractionOptions($bundle){
         $shde_options = [];
         $shde_terms = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadByProperties(
             [
-                'vid' => 'bill_test',
+                'vid' => $bundle,
             ]
         );
-
-        $shde_keys = array_keys($shde_terms); // {'a':1, 'b': 2} -> ['a','b']
+        $shde_keys = array_keys($shde_terms);
         foreach($shde_keys as $shde_key){
             $term = $shde_terms[$shde_key];
             $sdhe_options[$shde_key] = $term -> getName();        
         }
-
         return $sdhe_options;
     }
-
-    
 
     /**
     * {@inheritdoc}
@@ -33,8 +30,17 @@ class LabTestProfilesAdminForm extends FormBase {
 
     $form['#attached']['library'][] = 'cig_pods/lab_test_profiles_admin_form';
 
-    $sdhe = $this->getSoilHealthExtractionOptions();
-
+   
+    $agg_stab_method = $this->getSoilHealthExtractionOptions("d_aggregate_stability_me");
+    $agg_stab_unit = $this->getSoilHealthExtractionOptions("d_aggregate_stability_un");
+    $ec_method = $this->getSoilHealthExtractionOptions("d_ec_method");
+    $lab = $this->getSoilHealthExtractionOptions("d_laboratory");
+    $nitrate_method = $this->getSoilHealthExtractionOptions("d_nitrate_n_method");
+    $ph_method = $this->getSoilHealthExtractionOptions("d_ph_method");
+    $resp_detect = $this->getSoilHealthExtractionOptions("d_respiration_detection_");
+    $s_he_extract = $this->getSoilHealthExtractionOptions("d_soil_health_extraction");
+    
+    
     $form['lab_test_title'] = [
         '#markup' => '<h1>Lab Test Profiles</h1>',
     ]; 
@@ -48,126 +54,140 @@ class LabTestProfilesAdminForm extends FormBase {
     $form['laboratory'] = [
 			'#type' => 'select',
 			'#title' => 'Laboratory',
-			'#options' => $sdhe,
+			'#options' => $lab,
 			'#required' => TRUE
 		];
 
     $form['aggregate'] = [
 			'#type' => 'select',
 			'#title' => 'Aggregate Stability Method',
-			'#options' => $sdhe,
+			'#options' => $agg_stab_method,
 			'#required' => TRUE
 		];
 
-    $form['resp_incubation'] = [
+    $form['aggregate_unit'] = [
 			'#type' => 'select',
-			'#title' => 'Respiration Incubation Days',
-			'#options' => $sdhe,
+			'#title' => 'Aggregate Stability Unit',
+			'#options' => $agg_stab_unit,
 			'#required' => TRUE
 		];
+
+    // $form['resp_incubation'] = [
+	// 		'#type' => 'select',
+	// 		'#title' => 'Respiration Incubation Days',
+	// 		'#options' => $sdhe,
+	// 		'#required' => TRUE
+	// 	];
 
     $form['resp_detection'] = [
 			'#type' => 'select',
 			'#title' => 'Respiration Detection Method (unit ppm)',
-			'#options' => $sdhe,
+			'#options' => $resp_detect,
+			'#required' => TRUE
+		];
+
+    $form['pH_Method'] = [
+			'#type' => 'select',
+			'#title' => 'pH Method',
+			'#options' => $ph_method,
 			'#required' => TRUE
 		];
 
     $form['electr_method'] = [
         '#type' => 'select',
         '#title' => $this->t('Electroconductivity Method (EC (Unit dS/m))'),
-        '#options' => $sdhe,
+        '#options' => $ec_method,
         '#required' => TRUE
     ]; 
 
     $form['nitrate_method'] = [
         '#type' => 'select',
         '#title' => $this->t('Nitrate-N Method (Unit ppm)'),
-        '#options' => $sdhe,
+        '#options' => $nitrate_method,
         '#required' => TRUE
     ]; 
 
     $form['phos_method'] = [
         '#type' => 'select',
         '#title' => $this->t('Phosphorus Method (Unit ppm)'),
-        '#options' => $sdhe,
+        '#options' => $s_he_extract,
         '#required' => TRUE
     ]; 
 
     $form['potas_method'] = [
         '#type' => 'select',
         '#title' => $this->t('Potassium Method (Unit ppm)'),
-        '#options' => $sdhe,
+        '#options' => $s_he_extract,
         '#required' => TRUE
     ]; 
 
     $form['calc_method'] = [
         '#type' => 'select',
         '#title' => $this->t('Calcium Method (Unit ppm)'),
-        '#options' => $sdhe,
+        '#options' => $s_he_extract,
         '#required' => TRUE
     ]; 
 
     $form['magn_method'] = [
         '#type' => 'select',
         '#title' => $this->t('Magnesium Method (Unit ppm)'),
-        '#options' => $sdhe,
+        '#options' => $s_he_extract,
         '#required' => TRUE
     ]; 
 
     $form['sulf_method'] = [
         '#type' => 'select',
         '#title' => $this->t('Sulfur Method (Unit ppm)'),
-        '#options' => $sdhe,
+        '#options' => $s_he_extract,
         '#required' => TRUE
     ]; 
 
     $form['iron_method'] = [
         '#type' => 'select',
         '#title' => $this->t('Iron Method (Unit ppm)'),
-        '#options' => $sdhe,
+        '#options' => $s_he_extract,
         '#required' => TRUE
     ]; 
 
     $form['mang_method'] = [
         '#type' => 'select',
         '#title' => $this->t('Manganese Method (Unit ppm)'),
-        '#options' => $sdhe,
+        '#options' => $s_he_extract,
         '#required' => TRUE
     ]; 
 
     $form['cop_method'] = [
         '#type' => 'select',
         '#title' => $this->t('Copper Method (Unit ppm)'),
-        '#options' => $sdhe,
+        '#options' => $s_he_extract,
         '#required' => TRUE
     ]; 
 
     $form['zinc_method'] = [
         '#type' => 'select',
         '#title' => $this->t('Zinc Method (Unit ppm)'),
-        '#options' => $sdhe,
+        '#options' => $s_he_extract,
         '#required' => TRUE
     ]; 
 
     $form['boron_method'] = [
         '#type' => 'select',
         '#title' => $this->t('Boron Method (Unit ppm)'),
-        '#options' => $sdhe,
+        '#options' => $s_he_extract,
         '#required' => TRUE
     ]; 
 
     $form['alum_method'] = [
         '#type' => 'select',
         '#title' => $this->t('Aluminum Method (Unit ppm)'),
-        '#options' => $sdhe,
+        '#options' => $s_he_extract,
         '#required' => TRUE
     ]; 
 
     $form['moly_method'] = [
         '#type' => 'select',
         '#title' => $this->t('Molybdenum Methon (Unit ppm)'),
-        '#options' => $sdhe,
+        '#options' => $s_he_extract,
         '#required' => TRUE
     ]; 
 
@@ -205,11 +225,22 @@ class LabTestProfilesAdminForm extends FormBase {
     * {@inheritdoc}
     */
     public function submitForm(array &$form, FormStateInterface $form_state) {
-        $this
-            ->messenger()
-            ->addStatus($this
-            ->t('Form submitted for  @_name', [
-            '@_name' => $form['_name']['#value'],
-        ]));
-    }
+        // $this
+        //     ->messenger()
+        //     ->addStatus($this
+        //     ->t('Form submitted for  @_name', [
+        //     '@_name' => $form['nitrate_method']['#value'],
+        // ]));
+        // echo("<script>console.log('submit: " . $form['nitrate_method']['#value'] . "');</script>"); 
+//          $profile = []
+//  $profile['key_name'] = $form_state->getValue('form_elem_id')
+// ... Repeat for all fields
+
+// $profile['type'] = 'lab_testing_profile'
+// $profile['name'] = $form->getValue('form_name_elem,_id')
+
+// $asset = Asset::create($profile);
+// $asset->save();
+     }
 }
+//put inside submit function, fill in name

75aa057cb45499bde62d7ad0f601b755a65cdcfe
Marcos Carballal <marcoscrballal@spatialfront.com>
2022-06-07 15:06:48 -0400

    Producer Form Edit/Create is functional

--- web/modules/custom/cig_pods/src/Form/ProducerForm.php
+++ web/modules/custom/cig_pods/src/Form/ProducerForm.php
@@ -5,27 +5,81 @@ namespace Drupal\cig_pods\Form;
 Use Drupal\Core\Form\FormBase;
 Use Drupal\Core\Form\FormStateInterface;
 Use Drupal\asset\Entity\Asset;
+Use Drupal\Core\Url;
+
 
 
 class ProducerForm extends FormBase {
-
+	
+	// Returns id of page given path
+	private function pageLookup(string $path) {
+		$match = [];
+	   $path2 =  $path;
+	   $router = \Drupal::service('router.no_access_checks');
+   
+	   try {
+		 $match = $router->match($path2);
+	   }
+	   catch (\Exception $e) {
+		 // The route using that path hasn't been found,
+		 // or the HTTP method isn't allowed for that route.
+	   }
+	   return $match['_route'];
+	 }
+   
 
    /**
    * {@inheritdoc}
    */
-	public function buildForm(array $form, FormStateInterface $form_state, $options = NULL){
+	public function buildForm(array $form, FormStateInterface $form_state, $id = NULL){
+		$producer = [];
 		
-		$form['producer_name'] = [
+		$is_edit = $id <> NULL;
+		
+		if($is_edit){
+			$form_state->set('operation','edit');
+			$form_state->set('producer_id',$id);
+			$producer = \Drupal::entityTypeManager()->getStorage('asset')->load($id);
+		} else {
+			$form_state->set('operation','create');
+		}
+		
+
+		if($form_state->get('operation') == 'create'){
+			
+		}
+
+		// dpm($producer);
+
+		$producer_first_name_default_value = $is_edit ?  $producer->get('field_producer_first_name')->value : '';
+		$form['field_producer_first_name'] = [
 			'#type' => 'textfield',
-			'#title' => $this->t('Producer Name'),
-			'#description' => 'Producer Name',
+			'#title' => $this->t('Producer First Name'),
 			'#required' => TRUE,
+			'#default_value' => $producer_first_name_default_value,
 		];
+
+		$producer_last_name_default_value = $is_edit ?  $producer->get('field_producer_last_name')->value : '';
+		$form['field_producer_last_name'] = [
+			'#type' => 'textfield',
+			'#title' => $this->t('Producer Last Name'),
+			'#required' => TRUE,
+			'#default_value' => $producer_last_name_default_value,
+		]; 
 		
 		$form['actions']['send'] = [
 			'#type' => 'submit',
 			'#value' => $this->t('Send'),
 		  ];
+		
+
+		if($is_edit){
+			$form['actions']['delete'] = [
+				'#type' => 'submit',
+				'#value' => $this->t('Delete'),
+				'#submit' => ['::deleteProducer']
+			]; 			
+		}
 
 		
 		return $form;
@@ -37,6 +91,11 @@ class ProducerForm extends FormBase {
   public function validateForm(array &$form, FormStateInterface $form_state){
 	return;
 }
+
+public function deleteProducer(array &$form, FormStateInterface $form_state){
+	dpm("hello");
+}
+
 
   /**
    * {@inheritdoc}
@@ -50,12 +109,50 @@ class ProducerForm extends FormBase {
 	  '@producer_name' => $form['producer_name']['#value'],
 	]));
 
-	$asset = Asset::create([
-		'type' => 'producer',
-		'name' => $form['producer_name']['#value']
-	]);
+	$is_create = $form_state->get('operation') === 'create';
+	// dpm(gettype($operation));
 
-	$asset->save();
+	// PHP: '1' == 1 is True but '1' === 1 is False
+
+	if($is_create){
+		$producer_submission = [];
+		$producer_submission['field_producer_first_name'] = $form_state -> getValue('field_producer_first_name');
+		$producer_submission['field_producer_last_name'] = $form_state -> getValue('field_producer_last_name');
+		$producer_submission['type'] = 'producer';
+		$producer_submission['name'] = $producer_submission['field_producer_first_name']." ".$producer_submission['field_producer_last_name'];
+
+		$producer = Asset::create($producer_submission);
+		$producer->save();
+
+		$route = $this->pageLookup('/assets/producer');
+		$form_state->setRedirect($route);
+		// $url = Url::fromRoute($route);
+		// $form_state->setRedirectUrl($url);
+	} else {
+		$id = $form_state->get('producer_id');
+		$producer = \Drupal::entityTypeManager()->getStorage('asset')->load($id);
+		
+		$fn = $form_state->getValue('field_producer_first_name');
+		$ln = $form_state->getValue('field_producer_last_name');
+		$full_n = $fn." ".$ln;
+
+		$producer->set('field_producer_first_name', $fn);
+		$producer->set('field_producer_last_name', $ln);
+		$producer->set('name', $full_n);
+
+		$producer->save();
+		$route = $this->pageLookup('/assets/producer');
+		$form_state->setRedirect($route);
+
+	}
+	// Have to identify whether it is an edit or it is a create new
+
+	// $asset = Asset::create([
+	// 	'type' => 'producer',
+	// 	'name' => $form['producer_name']['#value']
+	// ]);
+
+	// $asset->save();
 	
 
   }
--- web/modules/custom/cig_pods/src/Plugin/Asset/AssetType/Producer.php
+++ web/modules/custom/cig_pods/src/Plugin/Asset/AssetType/Producer.php
@@ -3,6 +3,8 @@
 namespace Drupal\cig_pods\Plugin\Asset\AssetType;
 
 use Drupal\farm_entity\Plugin\Asset\AssetType\FarmAssetType;
+use Drupal\farm_field\FarmFieldFactory;
+
 /**
    * Provides the Producer asset type.
    *
@@ -17,5 +19,35 @@ use Drupal\farm_entity\Plugin\Asset\AssetType\FarmAssetType;
    * )
    */
 class Producer extends FarmAssetType {
+
+   public function buildFieldDefinitions() {
+      $fields = parent::buildFieldDefinitions();
+
+      $field_info = [
+         'field_producer_first_name' => [
+            'type' => 'string',
+            'label' => 'Producer First Name',
+            'description' => '',
+            'required' => TRUE,
+         ],
+         'field_producer_last_name' => [
+            'type' => 'string',
+            'label' => 'Producer Last Name',
+            'description' => '',
+            'required' => TRUE,
+         ]
+      ];
+
+      $farmFieldFactory = new FarmFieldFactory();
+      foreach($field_info as $name => $info){
+
+		$fields[$name] = $farmFieldFactory->bundleFieldDefinition($info)
+					      -> setDisplayConfigurable('form',TRUE)
+					      -> setDisplayConfigurable('view', TRUE);
+      }
+
+      return $fields;
+
+   }
 
 }

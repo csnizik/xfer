a2ce2b155fa5914abd683879d4266df0e23f5f6e
evan.kelley@spatialfront.com <evan.kelley@spaitalfront.com>
2022-07-18 10:25:16 -0600

    removed more comments

--- web/modules/custom/cig_pods/js/methods.js
+++ web/modules/custom/cig_pods/js/methods.js
@@ -1,7 +1,6 @@
 const select = document.getElementById("edit-actions-update-profile");
 
 select.addEventListener("mousedown", function handleChange(event) {
-    console.log("Clicked 5");
     this.style.display="none";
 
 })

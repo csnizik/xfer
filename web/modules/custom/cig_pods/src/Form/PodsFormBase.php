35ec4ba0b975a1970cf6dc8f37849c7b73f45402
Michael Stenta <mike@mstenta.net>
2022-08-31 15:51:23 -0400

    Use the pods_asset_er View for asset entity lists in PodsFormBase::entityOptions().

--- web/modules/custom/cig_pods/src/Form/PodsFormBase.php
+++ web/modules/custom/cig_pods/src/Form/PodsFormBase.php
@@ -41,11 +41,25 @@ class PodsFormBase extends FormBase {
    */
   protected function entityOptions(string $entity_type, string $bundle) {
     $options = [];
+
+    // If the entity type is "asset", use our custom Entity Reference View to
+    // get the list of options. Otherwise, use the default selection handler.
     $selection_options = [
       'handler' => 'default:' . $entity_type,
       'target_type' => $entity_type,
       'target_bundles' => [$bundle],
     ];
+    if ($entity_type == 'asset') {
+      $selection_options = [
+        'handler' => 'views',
+        'target_type' => $entity_type,
+        'view' => [
+          'view_name' => 'pods_asset_er',
+          'display_name' => 'entity_reference',
+          'arguments' => [$bundle],
+        ],
+      ];
+    }
     $selection_handler = \Drupal::service('plugin.manager.entity_reference_selection')->getInstance($selection_options);
     $referenceable_entities = $selection_handler->getReferenceableEntities();
     if (!empty($referenceable_entities[$bundle])) {

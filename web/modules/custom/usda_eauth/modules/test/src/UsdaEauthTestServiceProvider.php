6c69b5b9ca34bb7e717578e537d9232cbc16e40e
Michael Stenta <mike@mstenta.net>
2022-09-01 14:11:50 -0400

    Override the usda_eauth.zroles service class with a testing class via usda_eauth_test module.

---
+++ web/modules/custom/usda_eauth/modules/test/src/UsdaEauthTestServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drupal\usda_eauth_test;
+
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\Core\DependencyInjection\ServiceProviderBase;
+use Drupal\Core\DependencyInjection\ServiceProviderInterface;
+
+/**
+ * Override the usda_eauth.zroles service with a testing class.
+ */
+class UsdaEauthTestServiceProvider extends ServiceProviderBase implements ServiceProviderInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alter(ContainerBuilder $container) {
+    $definition = $container->getDefinition('usda_eauth.zroles');
+    $definition->setClass('Drupal\usda_eauth_test\zRolesUtilitiesTest');
+  }
+
+}

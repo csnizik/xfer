92cc03adab45db40a094369f6b48f9e640523393
Sundararaj Kumarasamy <Sundararaj.Kumarasamy@usda.gov>
2023-02-10 09:42:57 -0600

    Pull request #16: Import of multiple other costs via csv
    Merge in NRCSAPPS/pods from story/CIGCPDPH2-767-poc-import-of-other-costs-in-pods-via-.csv to develop
    
    Squashed commit of the following:
    
    commit 1c107a0c3cb47b1ff756ad50dbde566690c800f6
    Author: sundar_kumarasamy1 <sundar.kumarasamy@spatialfront.com>
    Date:   Fri Feb 10 09:57:42 2023 -0500
    
        Change names to functional
    
    commit 0a36ef26e0e2aeb2c954a1b0c33d461ba906680c
    Author: sundar_kumarasamy1 <sundar.kumarasamy@spatialfront.com>
    Date:   Thu Feb 9 10:55:08 2023 -0500
    
        Import of multiple other costs via csv

--- web/modules/custom/csv_import/src/Controller/CsvImportController.php
+++ web/modules/custom/csv_import/src/Controller/CsvImportController.php
@@ -14,7 +14,7 @@ class CsvImportController extends ControllerBase {
    * @return array
    *   A simple renderable array.
    */
-  public function test() {
+  public function upload() {
     return [
       '#children' => '
         inputs:
@@ -33,11 +33,42 @@ class CsvImportController extends ControllerBase {
   }
 
 
-  public function process() {
+  public function inputs() {
     $file = \Drupal::request()->files->get("file");
     $fName = $file->getClientOriginalName();
     $fLoc = $file->getRealPath();
     $csv = array_map('str_getcsv', file($fLoc));
+
+    $oc_index =  array_search("other_costs",$csv[0]);
+
+    $csv_oc = $csv[1][$oc_index];
+
+    $result = str_replace( '"', '', $csv_oc);
+
+    $exps = explode("|",$result);
+
+    $csid = [];
+    
+    foreach( $exps as $exp){
+      
+      $cval = explode(",",$exp);
+      $cost = $cval[0];
+      $cost_type = $cval[1];
+      //create cost sequence
+      $cost_sequence = [];
+      $cost_sequence['type'] = 'cost_sequence';
+      $cost_sequence['field_cost_type'] = ['target_id' => $cost_type];
+      $cost_sequence['field_cost'] = $cost;
+      $cost_sequenceN = Asset::create($cost_sequence);
+
+      $cost_sequenceN->save();
+
+      $nid = $cost_sequenceN->id();
+
+      $csid[] = $nid;
+
+    }
+
 
 
     $operation = \Drupal::entityTypeManager()->getStorage('asset')->load($csv[1][0]);
@@ -53,26 +84,17 @@ class CsvImportController extends ControllerBase {
     $input_submission['field_cost_per_unit'] = $csv[1][6];
     $input_submission['field_custom_application_unit'] = $csv[1][7];
     $input_submission['project'] = $project;
+    $input_submission['field_input_cost_sequences'] = $csid;
 
     $operation_taxonomy_name = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->load($operation->get('field_operation')->target_id);
     $input_taxonomy_name = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->load($csv[1][2]);
     $input_submission['name'] = $operation_taxonomy_name->getName() . "_" . $input_taxonomy_name->getName() . "_" . $csv[1][1];
-
     
     $input_to_save = Asset::create($input_submission);
 
-    $cost_submission = [];
-    $cost_submission ['type'] = 'cost_sequence';
-    $cost_submission ['field_cost_type'] = $csv[1][8];
-    $cost_submission ['field_cost'] = $csv[1][9];
-
-    $other_cost = Asset::create($cost_submission);
-
-    $input_to_save->set('field_input_cost_sequences', $other_cost);
     $input_to_save->save();
     
-    $operation->get('field_input')[] = $input_to_save->id();
-    $operation->save();
+
 
     return [
       "#children" => "uploaded.",
@@ -80,11 +102,42 @@ class CsvImportController extends ControllerBase {
     
   }
 
-  public function process2() {
+  public function operations() {
+
     $file = \Drupal::request()->files->get("file");
     $fName = $file->getClientOriginalName();
     $fLoc = $file->getRealPath();
     $csv = array_map('str_getcsv', file($fLoc));
+
+    $oc_index =  array_search("other_costs",$csv[0]);
+
+    $csv_oc = $csv[1][$oc_index];
+
+    $result = str_replace( '"', '', $csv_oc);
+
+    $exps = explode("|",$result);
+
+    $csid = [];
+
+    foreach( $exps as $exp){
+      
+      $cval = explode(",",$exp);
+      $cost = $cval[0];
+      $cost_type = $cval[1];
+      //create cost sequence
+      $cost_sequence = [];
+      $cost_sequence['type'] = 'cost_sequence';
+      $cost_sequence['field_cost_type'] = ['target_id' => $cost_type];
+      $cost_sequence['field_cost'] = $cost;
+      $cost_sequenceN = Asset::create($cost_sequence);
+
+      $cost_sequenceN->save();
+
+      $nid = $cost_sequenceN->id();
+
+      $csid[] = $nid;
+
+    }
 
     $shmu = \Drupal::entityTypeManager()->getStorage('asset')->load($csv[1][0]);
     $project = \Drupal::entityTypeManager()->getStorage('asset')->load($shmu->get('project')->target_id);
@@ -103,20 +156,12 @@ class CsvImportController extends ControllerBase {
     $operation_submission['field_tractor_self_propelled_machine'] = $csv[1][5];
     $operation_submission['field_row_number'] = $csv[1][6];
     $operation_submission['field_width'] = $csv[1][7];
-    $operation_submissionoperation_submission['field_horsepower'] = $csv[1][8];
+    $operation_submission['field_horsepower'] = $csv[1][8];
     $operation_submission['project'] = $project;
-
+    $operation_submission['field_operation_cost_sequences'] = $csid;
     $operation_to_save = Asset::create($operation_submission);
 
-    // $cost_submission = [];
-    // $cost_submission ['type'] = 'cost_sequence';
-    // $cost_submission ['field_cost_type'] = $csv[1][9];
-    // $cost_submission ['field_cost'] = $csv[1][8];
 
-    // $other_cost = Asset::create($cost_submission);
-
-    // $operation_to_save->set('field_operation_cost_sequences', $other_cost);
-    
     $operation_to_save->save();
 
     return [
